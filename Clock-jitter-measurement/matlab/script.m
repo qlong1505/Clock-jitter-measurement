e6b83ab59634eacc458d5c633a3b60819e1a9b39
qlong1505 <qlong1505@gmail.com>
2018-12-05 19:20:54 +1100

    add data measured from beaglebone

--- Clock-jitter-measurement/matlab/script.m
+++ Clock-jitter-measurement/matlab/script.m
@@ -1,8 +1,32 @@
-file = 'data.csv';
-x = csvread(file);
+file = 'acq0003.csv';
+% x = csvread(file);
+x = readtable(file)
+%%
 histogram(x,1000)
 mean
 %%
 meanx = mean(x);
 x = x(x<1.1*meanx & x>0.9*meanx);
 histogram(x,30);
+
+%% histogram of hardware measure jitter
+file = {'acq0001.csv',...
+    'acq0002.csv',...
+    'acq0003.csv',...
+    'acq0004.csv',...
+    'acq0005.csv',...
+    'acq0006.csv',...
+    'acq0007.csv',...
+    'acq0008.csv',...
+    'acq0009.csv',...
+    'acq0010.csv'};
+total_jitter=[];
+for i=1:length(file)
+    data=readtable(file{i})
+    V = data.Channel1_V_;
+    T = data.Time_s_;
+    jitter = process(V,T);
+    total_jitter = [total_jitter;jitter];
+end
+histogram(total_jitter(total_jitter<0.5),50);
+%%
