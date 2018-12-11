737858edfdcdee398e6605f8f22f8339f20d418a
qlong1505 <qlong1505@gmail.com>
2018-12-11 16:17:49 +1100

    add data measured from beagle bone

--- Clock-jitter-measurement/matlab/script.m
+++ Clock-jitter-measurement/matlab/script.m
@@ -10,16 +10,15 @@ x = x(x<1.1*meanx & x>0.9*meanx);
 histogram(x,30);
 
 %% histogram of hardware measure jitter
-file = {'acq0001.csv',...
-    'acq0002.csv',...
-    'acq0003.csv',...
-    'acq0004.csv',...
-    'acq0005.csv',...
-    'acq0006.csv',...
-    'acq0007.csv',...
-    'acq0008.csv',...
-    'acq0009.csv',...
-    'acq0010.csv'};
+folder ={'1ms_bb_high_priority','1ms_bb_normal_priority','1ms_bb_low_priority';...
+    '1ms_rasp_high_priority','1ms_rasp_normal_priority','1ms_rasp_low_priority'}
+
+Q1 = 'Which platform?\n1.Beagle Bone Black (single core)\n2.Raspberry pi (multi cores)\n';
+x = input(Q1);
+Q2 ='Choose priority level:\n1.High\n2.Normal\n3.Low\n'
+y =input(Q2);
+file = listACQ(folder{x,y});
+
 total_jitter=[];
 for i=1:length(file)
     data=readtable(file{i})
@@ -28,5 +27,37 @@ for i=1:length(file)
     jitter = process(V,T);
     total_jitter = [total_jitter;jitter];
 end
-histogram(total_jitter(total_jitter<0.5),50);
+histogram(total_jitter,50);
+set(gca,'YScale','log')
+hold on
+
+% normal distribution with mean is 1ms
+%     x = normrnd(0.001,std(total_jitter),length(total_jitter),1);
+%     histogram(x,50);
+%     set(gca,'YScale','log')
+
+% Normal distribution with mean is data mean
+x = normrnd(mean(total_jitter),std(total_jitter),length(total_jitter),1);
+histogram(x,50);
+set(gca,'YScale','log')
+
+% Half normal distribution
+%         pd2 = makedist('HalfNormal','mu',mean(total_jitter),'sigma',std(total_jitter));
+%         x = random(pd2,length(total_jitter),1)
+%         histogram(x,50);
+%         set(gca,'YScale','log')
+
+hold off
 %%
+Q1 = 'Which platform?\n1.Beagle Bone Black (single core)\n2.Raspberry pi (multi cores)\n';
+x = input(Q1);
+Q2 ='Choose priority level:\n1.High\n2.Normal\n3.Low'
+y =input(Q2);
+folder{x,y}
+%%
+folder ={'1ms_bb_high_priority','1ms_bb_normal_priority','1ms_bb_low_priority';...
+    '1ms_rasp_high_priority','1ms_rasp_normal_priority','1ms_rasp_low_priority'}
+
+%%
+pd2 = makedist('HalfNormal','mu',mean(total_jitter),'sigma',std(total_jitter));
+x = random(pd2,length(total_jitter),1)
