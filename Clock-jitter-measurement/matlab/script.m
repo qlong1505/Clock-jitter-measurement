7bdfaaef9f39f4e4f1db76d9cca8d6148446fa79
qlong1505 <qlong1505@gmail.com>
2018-12-12 18:43:13 +1100

    add motor open loop response

--- Clock-jitter-measurement/matlab/script.m
+++ Clock-jitter-measurement/matlab/script.m
@@ -29,11 +29,17 @@ for i=1:length(file)
         V = data.Channel2_V_;
     end
     T = data.Time_s_;
-    jitter = process(V,T);
+    jitter  = process(V,T);
     total_jitter = [total_jitter;jitter];
 end
+
+clk = clock_generator(total_jitter);
+
+
 histogram(total_jitter,50);
 set(gca,'YScale','log')
+xlabel('Value') 
+ylabel('Counts') 
 hold on
 
 % normal distribution with mean is 1ms
@@ -42,9 +48,9 @@ hold on
 %     set(gca,'YScale','log')
 
 % Normal distribution with mean is data mean
-x = normrnd(mean(total_jitter),std(total_jitter),length(total_jitter),1);
-histogram(x,50);
-set(gca,'YScale','log')
+%     x = normrnd(mean(total_jitter),std(total_jitter),length(total_jitter),1);
+%     histogram(x,50);
+%     set(gca,'YScale','log')
 
 % Half normal distribution
 %         pd2 = makedist('HalfNormal','mu',mean(total_jitter),'sigma',std(total_jitter));
@@ -53,6 +59,9 @@ set(gca,'YScale','log')
 %         set(gca,'YScale','log')
 
 hold off
+formatOut = 'yyyymmddHHMMSS';
+print(strcat('hist_',folder{x,y},datestr(now,formatOut)),'-dpng')
+save(strcat('clk_',folder{x,y},datestr(now,formatOut)),'clk')
 %%
 Q1 = 'Which platform?\n1.Beagle Bone Black (single core)\n2.Raspberry pi (multi cores)\n';
 x = input(Q1);
@@ -66,3 +75,46 @@ folder ={'1ms_bb_high_priority','1ms_bb_normal_priority','1ms_bb_low_priority';.
 %%
 pd2 = makedist('HalfNormal','mu',mean(total_jitter),'sigma',std(total_jitter));
 x = random(pd2,length(total_jitter),1)
+
+%% open loop step response - load data
+OLdata=readtable('open_loop.csv');
+%%
+load('open_loop.mat');
+%%
+T = OLdata.Time_s_(OLdata.Time_s_>=0);
+EncoderA = OLdata.Channel1_V_(find(OLdata.Time_s_>=0));
+EncoderB = OLdata.Channel2_V_(find(OLdata.Time_s_>=0));
+%
+EncoderA(EncoderA<1.5)=0;
+EncoderA(EncoderA>=1.5)=1;
+EncoderA = diff(EncoderA);
+EncoderA(EncoderA==-1)=1;
+
+EncoderB(EncoderB<1.5)=0;
+EncoderB(EncoderB>=1.5)=1;
+EncoderB = diff(EncoderB);
+EncoderB(EncoderB==-1)=1;
+
+T1 = T(find(EncoderA==1));
+T2 = T(find(EncoderB==1));
+%
+T = zeros(length(T1)+length(T2),1);
+T(1:2:end)=T1;
+T(2:2:end)=T2;
+%
+T = diff(T);
+T = diff(T2);
+%
+Speed = T*24;
+%
+Speed = 1./Speed;
+Speed = Speed*60; %rpm
+%
+T = cumsum(T);
+%
+plot(T(T<0.5),Speed(find(T<0.5)));
+%%
+K = 4543/5;
+tau = 0.042
+sys = tf(K,[tau 1])
+step(sys*5)
