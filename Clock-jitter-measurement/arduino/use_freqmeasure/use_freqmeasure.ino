1e705ed3f9641d0d7b1684c32733cf81f65d8046
Long Tran <qlong1505@gmail.com>
2018-12-31 23:15:43 +1100

    add Arduino program using Freqmeasure library

---
+++ Clock-jitter-measurement/arduino/use_freqmeasure/use_freqmeasure.ino
@@ -0,0 +1,25 @@
+#include <FreqMeasure.h>
+
+void setup() {
+  Serial.begin(115200);
+  FreqMeasure.begin();
+}
+
+double sum=0;
+int count=0;
+
+void loop() {
+  if (FreqMeasure.available()) {
+    // average several reading together
+    sum = sum + FreqMeasure.read();
+    count = count + 1;
+    if (count > 1) {
+      float frequency = FreqMeasure.countToFrequency(sum / count);
+      //float frequency = FreqMeasure.countToNanoseconds(sum / count);
+      Serial.println(frequency,5);
+      //Serial.println(F_CPU);
+      sum = 0;
+      count = 0;
+    }
+  }
+}
