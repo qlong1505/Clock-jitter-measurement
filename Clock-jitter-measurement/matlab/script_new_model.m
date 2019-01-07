54b54af09961999827349002f32f5ac426763007
qlong1505 <qlong1505@gmail.com>
2019-01-07 15:23:49 +1100

    no commit new modifed mat and png file

--- Clock-jitter-measurement/matlab/script_new_model.m
+++ Clock-jitter-measurement/matlab/script_new_model.m
@@ -3,8 +3,8 @@
 % Simulate model from Chapter 13, page 552, 
 Ts = 1/120
 z = tf('z',Ts);
-% Gc = 150*(z-0.72)/(z+0.4);
-Gc = 90*(z-0.72)/z;
+Gc = 150*(z-0.72)/(z+0.4);
+%Gc = 90*(z-0.72)/z;
 % Gc = 90*(z-0.72)/z;
 Gp = 0.00133*(z+0.75)/(z*(z-1)*(z-0.72));
 cl = feedback(Gc*Gp,1);
@@ -31,7 +31,7 @@ runtime = 0.2;
 n_run = 10000;
 
 %load clk source from hardware
-file ='clk_Opi_1_120s_norm20181225230330.mat'; 
+file ='clk_BB_1_120s_norm20181225221250.mat'; 
 load(file);
 
 %split clock source into different pattern enough for runtime.
@@ -81,7 +81,8 @@ load(file);
 tic
 hold on
 count=0
-for i=2:length(response(1,:))
+for i=2:5%length(response(1,:))
+    
     plot(response(:,1),response(:,i));
     count = count +1
 end
@@ -101,3 +102,115 @@ clear response
 formatOut = 'yyyymmddHHMMSS';
 print(file(1:(end-4)),'-dpng')
 toc
+%% find stepinfo and save to csv file.
+MEAN=[];
+STD=[];
+
+n=1;
+data_risetime = [];
+data_settletime = [];
+for file_list=[...
+        "response_clk_BB_1_120s_high20181225215231.mat" ...
+        "response_clk_BB_1_120s_low20181225225238.mat" ...
+        "response_clk_BB_1_120s_norm20181225221250.mat" ...
+        "response_clk_Opi_1_120_high20181225233058.mat" ...
+        "response_clk_Opi_1_120_low20181225230436.mat"...
+        "response_clk_Opi_1_120s_norm20181225230330.mat"...
+        ]
+    file =convertStringsToChars(file_list)
+    header = "RiseTime,SettlingMax,SettlingMin,SettlingTime,Peak,PeakTime,Overshoot,Undershoot\n";
+    fileID = fopen(strcat(file(1:(end-4)),'.csv'),'w');
+    fprintf(fileID,header);
+    fclose(fileID);
+    clear fileID;
+    clear header;
+    pause(2);
+    tic
+    load(file);
+    toc
+    pause(2);
+    tic
+    data_size = size(response,2)-1;
+    
+    RiseTime=zeros(1,data_size);
+    SettlingTime=zeros(1,data_size);
+    SettlingMin=zeros(1,data_size);
+    SettlingMax=zeros(1,data_size);
+    Overshoot=zeros(1,data_size);
+    Undershoot=zeros(1,data_size);
+    Peak=zeros(1,data_size);
+    PeakTime=zeros(1,data_size);   
+    for i=1:data_size
+        s= stepinfo(response(:,i+1),response(:,1));   
+        RiseTime(i) = s.RiseTime;
+        SettlingMax(i)=s.SettlingMax;
+        SettlingMin(i)=s.SettlingMin;
+        SettlingTime(i)=s.SettlingTime;
+        Peak(i)=s.Peak;
+        PeakTime(i)=s.PeakTime;
+        Overshoot(i)=s.Overshoot;
+        Undershoot(i)=s.Undershoot;
+    end
+    toc
+    data_settletime = [data_settletime,SettlingTime'];
+    data_risetime = [data_risetime,RiseTime'];
+    formatOut = 'yyyymmddHHMMSS';
+     %csvwrite(strcat(file(1:(end-4)),'.csv'),overshoots);
+%     dlmwrite(strcat(file(1:(end-4)),'.csv'),[RiseTime',SettlingMax'...
+%    SettlingMin', SettlingTime',Peak',PeakTime',Overshoot',Undershoot']...
+%     ,'delimiter',',','-append');
+dlmwrite(strcat(file(1:(end-4)),'.csv'),[RiseTime',SettlingMax'...
+   SettlingMin', SettlingTime',Peak',PeakTime',Overshoot',Undershoot']...
+    ,'delimiter',',','-append');
+
+    subplot(3,4,n);
+    histogram(SettlingTime,100);
+    set(gca,'YScale','log')
+    title(strcat('SettlingTime_',file(1:(end-4))),'Interpreter','none');
+    n= n+1;
+    subplot(3,4,n);
+    histogram(RiseTime,200);
+    set(gca,'YScale','log')
+    title(strcat('RiseTime_',file(1:(end-4))),'Interpreter','none');
+    n = n+1;
+    %MEAN=[MEAN,mean(overshoots)];
+    %STD=[STD,std(overshoots)];
+end
+figure;
+subplot(2,1,1);
+boxplot(data_risetime,'Labels',{'BeagleBone - high priority','BeagleBone - low priority',...
+    'BeagleBone - default priority','Orange Pi - high priority','Orange Pi - low priority',...
+    'Orange Pi - normal priority'},'orientation', 'horizontal');
+title('Risetime');
+set(gca,'FontSize',20)
+subplot(2,1,2);
+boxplot(data_settletime,'Labels',{'BeagleBone - high priority','BeagleBone - low priority',...
+    'BeagleBone - default priority','Orange Pi - high priority','Orange Pi - low priority',...
+    'Orange Pi - normal priority'},'orientation', 'horizontal');title('Settletime');
+set(gca,'FontSize',20)
+clear RiseTime SettlingMax SettlingMin SettlingTime Peak PeakTime Overshoot Undershoot
+clear s data_size i file file_list
+clear MEAN STD
+clear response
+clear n
+%% boxplot data
+subplot(2,1,1);
+boxplot(data_risetime,'Labels',{'BeagleBone - high priority','BeagleBone - low priority',...
+    'BeagleBone - default priority','Orange Pi - high priority','Orange Pi - low priority',...
+    'Orange Pi - normal priority'},'orientation', 'horizontal');
+title('Risetime');
+xlabel('Time (s)');
+set(gca,'FontSize',20)
+subplot(2,1,2);
+boxplot(data_settletime,'Labels',{'BeagleBone - high priority','BeagleBone - low priority',...
+    'BeagleBone - default priority','Orange Pi - high priority','Orange Pi - low priority',...
+    'Orange Pi - normal priority'},'orientation', 'horizontal');
+title('Settletime');
+xlabel('Time (s)');
+set(gca,'FontSize',20)
+
+%% get ideal data
+runtime = 0.2;
+sim('Tape_motion_dynamic_ideal');
+plot(ScopeData1.time, ScopeData1.signals.values);
+s=stepinfo(ScopeData1.signals.values,ScopeData1.time);
